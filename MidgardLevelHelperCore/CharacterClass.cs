db652fda1d54bc26ec971fb112dc81cce502ed69
Damian Karaszewski <51860737+Sewenda@users.noreply.github.com>
2022-09-15 23:39:17 +0200

    Created class CharacterClass

---
+++ MidgardLevelHelperCore/CharacterClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardLevelHelperCore
+{
+    public class CharacterClass
+    {
+        public CharacterClass(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            Name = name;
+        }
+
+        public string Name { get; }
+        public LevelingSchema LevelingSchema { get; init; }
+        //TODO Grundfähigkeit & Ausnahmefähigkeit definieren
+    }
+}
