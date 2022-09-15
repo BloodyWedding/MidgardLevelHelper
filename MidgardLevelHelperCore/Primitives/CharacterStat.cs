4f8df35a0433e11cd5e5c498971c17c5fff286ef
Damian Karaszewski <51860737+Sewenda@users.noreply.github.com>
2022-09-15 23:38:50 +0200

    Created primitives CharacterStat and class Skill

---
+++ MidgardLevelHelperCore/Primitives/CharacterStat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardLevelHelperCore.Primitives
+{
+    public class CharacterStat
+    {
+        public CharacterStat(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            Name = name;
+        }
+
+        public string Name { get; }
+        public uint Value { get; init; }
+    }
+}
---
+++ MidgardLevelHelperCore/Skill.cs
@@ -0,0 +1,18 @@
+﻿using MidgardLevelHelperCore.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardLevelHelperCore
+{
+    public class Skill
+    {
+        public string Name { get; }
+        public IReadOnlyList<CharacterStat> Requirements{ get; init; }
+        public byte StartingLevel { get; init; }
+        public bool CanImprove { get; init; }
+        public TotalExperiencePoints Cost { get; init; }
+    }
+}
