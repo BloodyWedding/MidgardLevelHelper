5c86f48c0e9a96b75a57be62c0ae95d6e40fa1f9
Damian Karaszewski <51860737+Sewenda@users.noreply.github.com>
2022-09-22 23:19:38 +0200

    Added Resistances, BaseSkills, ExceptionalSkills and ForbiddenSkills

---
+++ MidgardLevelHelperCore/Character.cs
@@ -0,0 +1,26 @@
+﻿using MidgardLevelHelperCore.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardLevelHelperCore
+{
+    public class Character
+    {
+        public Character(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            Name = name;
+        }
+
+        public string Name { get; }
+        public CharacterClass Class { get; init; }
+        public List<EditableCharacterStat> Skills { get; init; }
+        public IReadOnlyList<EditableCharacterStat> Attributes { get; init; }
+    }
+}
---
+++ MidgardLevelHelperCore/Primitives/Resistances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardLevelHelperCore.Primitives
+{
+    public struct Resistances
+    {
+        public byte Psychic { get; init; }
+        public byte Physical { get; init; }
+        public byte Environmental { get; init; }
+    }
+}
