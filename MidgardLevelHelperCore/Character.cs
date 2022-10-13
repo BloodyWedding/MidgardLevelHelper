f98fa06ec2c1d82ecd479b5a5ff3933e4e1c500b
hummigbird1 <hummigbird1@gmail.com>
2022-10-13 22:28:19 +0200

    Add prevention of duplicate entries in Character.Skills and Attributes (related to Github issue #1)

--- MidgardLevelHelperCore/Character.cs
+++ MidgardLevelHelperCore/Character.cs
@@ -1,17 +1,18 @@
-﻿using MidgardLevelHelperCore.Primitives;
+﻿using MidgardLevelHelperCore.Comparer;
+using MidgardLevelHelperCore.Primitives;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MidgardLevelHelperCore
 {
     public class Character
     {
+        private IReadOnlyList<EditableCharacterStat> _attributes;
+
         public Character(string name)
         {
-            if(string.IsNullOrWhiteSpace(name))
+            if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentNullException(nameof(name));
             }
@@ -19,9 +20,31 @@ namespace MidgardLevelHelperCore
         }
 
         public string Name { get; }
-        public CharacterClass Class { get; init; }
+        public CharacterClass Class { get; init; } // TODO Prevent NULL
         public short HealthPoints { get; init; }
-        public List<EditableCharacterStat> Skills { get; init; }
-        public IReadOnlyList<EditableCharacterStat> Attributes { get; init; }
+        public HashSet<EditableCharacterStat> Skills { get; } = new HashSet<EditableCharacterStat>(CharacterStatNameComparer.Default);
+        public IReadOnlyList<EditableCharacterStat> Attributes
+        {
+            get => _attributes;
+
+            init
+            {
+                ArgumentNullException.ThrowIfNull(nameof(value));
+                EnsureUniqueAttributes(value);
+                _attributes = value;
+            }
+        }
+
+        private void EnsureUniqueAttributes(IReadOnlyList<EditableCharacterStat> value)
+        {
+            var duplicates = value.GroupBy(stat => stat, CharacterStatNameComparer.Default)
+                .Where(group => group.Count() > 1)
+                .Select(x => x.Key.Name)
+                .ToArray();
+            if (duplicates.Any())
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), $"The following Attributes were specified multiple times: {string.Join(", ", duplicates)}");
+            }
+        }
     }
 }
