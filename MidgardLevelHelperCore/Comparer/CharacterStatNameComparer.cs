6226aca2774b562aab2c68c158e88f0404803711
hummigbird1 <hummigbird1@gmail.com>
2022-10-13 22:22:40 +0200

    Add CharacterStatNameComparer

---
+++ MidgardLevelHelperCore/Comparer/CharacterStatNameComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using MidgardLevelHelperCore.Primitives;
+
+namespace MidgardLevelHelperCore.Comparer
+{
+    public class CharacterStatNameComparer : IEqualityComparer<CharacterStat>
+    {
+        public static CharacterStatNameComparer Default { get; } = new CharacterStatNameComparer();
+        public bool Equals(CharacterStat x, CharacterStat y)
+        {
+            if (x is not null && y is not null)
+            {
+                return Equals(x.Name, y.Name);
+            }
+
+            return x == null && y == null;
+        }
+
+        public int GetHashCode([DisallowNull] CharacterStat obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
