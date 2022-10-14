16bbde87574b63a0c8252c6e3d74df0f26e09ddb
hummigbird1 <hummigbird1@gmail.com>
2022-10-14 22:10:13 +0200

    Add SkillsAndAttributesComparer

---
+++ MidgardLevelHelperCore/Comparer/SkillsAndAttributesComparer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MidgardLevelHelperCore.Comparer
+{
+    public static class SkillsAndAttributesComparer
+    {
+        public static StringComparer Default { get; } = StringComparer.CurrentCultureIgnoreCase;
+    }
+}
