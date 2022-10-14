760fb4f8fd34f9c7ea8913992035b0d60dc4b43f
hummigbird1 <hummigbird1@gmail.com>
2022-10-14 22:13:13 +0200

    Add LevelingSchemaTests

---
+++ MidgardLevelHelperCore.Tests/LevelingSchemaTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace MidgardLevelHelperCore.Tests
+{
+    public class LevelingSchemaTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Constructor_TypeCanNotBeNullOrWhiteSpace_ThrowsArgumentNullException(string type)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _ = new LevelingSchema(type));
+            Assert.Equal("type", exception.ParamName);
+        }
+    }
+}
