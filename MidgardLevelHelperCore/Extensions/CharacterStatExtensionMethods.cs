2ab2e82784c0b567295475848d6957144086bc44
hummigbird1 <hummigbird1@gmail.com>
2022-10-14 22:11:18 +0200

    Add CharacterStatExtensionMethods

---
+++ MidgardLevelHelperCore/Extensions/CharacterStatExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using MidgardLevelHelperCore.Comparer;
+using MidgardLevelHelperCore.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MidgardLevelHelperCore.Extensions
+{
+    internal static class CharacterStatExtensionMethods
+    {
+        public static string[] GetNonUniqueCharacterStatNames<T>(this IEnumerable<T> characterStats) where T : CharacterStat
+        {
+            ArgumentNullException.ThrowIfNull(characterStats);
+
+            return characterStats.GroupBy(stat => stat, CharacterStatNameComparer.Default)
+                .Where(group => group.Count() > 1)
+                .Select(x => x.Key.Name)
+                .ToArray();
+        }
+    }
+}
