d708bccc73fb02f542516a6540b061733a90bdf2
hummigbird1 <hummigbird1@gmail.com>
2022-10-14 22:18:31 +0200

    - Modify CharacterClass to prevent NULL and duplicates in BaseSkills,ExceptionalSkills and  ForbiddenSkills - Prevent NULL in LevelingSchema - Add Unit Tests (for Github issue #1)

--- MidgardLevelHelperCore/CharacterClass.cs
+++ MidgardLevelHelperCore/CharacterClass.cs
@@ -1,14 +1,21 @@
-﻿using MidgardLevelHelperCore.Primitives;
+﻿using MidgardLevelHelperCore.Extensions;
+using MidgardLevelHelperCore.Primitives;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MidgardLevelHelperCore
 {
     public class CharacterClass
     {
+        private readonly LevelingSchema levelingSchema;
+        private readonly IReadOnlyList<string> _baseSkills;
+        private readonly IReadOnlyList<string> _exceptionalSkills;
+        private readonly IReadOnlyList<string> _forbiddenSkills;
+
         public CharacterClass(string name)
         {
-            if(string.IsNullOrWhiteSpace(name))
+            if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentNullException(nameof(name));
             }
@@ -16,10 +23,66 @@ namespace MidgardLevelHelperCore
         }
 
         public string Name { get; }
-        public LevelingSchema LevelingSchema { get; init; } // TODO Prevent NULL
+        public LevelingSchema LevelingSchema
+        {
+            get => levelingSchema;
+            init
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                levelingSchema = value;
+            }
+        }
         public Resistances ResistanceBonuses { get; init; }
-        public IReadOnlyList<string> BaseSkills { get; init; } // TODO Prevent duplicate entries and NULL
-        public IReadOnlyList<string> ExceptionalSkills { get; init; } // TODO Prevent duplicate entries and NULL
-        public IReadOnlyList<string> ForbiddenSkills { get; init; } // TODO Prevent duplicate entries and NULL
+        public IReadOnlyList<string> BaseSkills
+        {
+            get => _baseSkills;
+            init
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                EnsureContainsNoNullOrWhiteSpaces(value, nameof(value));
+                var duplicates = value.GetNonUniqueNames();
+                if (duplicates.Length > 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"The following skills were specified multiple times: {string.Join(", ", duplicates)}");
+
+                // TODO Need to ensure that added skill does not already exist in ExceptionalSkills or ForbiddenSkill
+                _baseSkills = value;
+            }
+        }
+        public IReadOnlyList<string> ExceptionalSkills
+        {
+            get => _exceptionalSkills;
+            init
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                EnsureContainsNoNullOrWhiteSpaces(value, nameof(value));
+                var duplicates = value.GetNonUniqueNames();
+                if (duplicates.Length > 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"The following skills were specified multiple times: {string.Join(", ", duplicates)}");
+
+                // TODO Need to ensure that added skill does not already exist in BaseSkills or ForbiddenSkill
+                _exceptionalSkills = value;
+            }
+        }
+        public IReadOnlyList<string> ForbiddenSkills
+        {
+            get => _forbiddenSkills;
+            init
+            {
+                ArgumentNullException.ThrowIfNull(value);
+                EnsureContainsNoNullOrWhiteSpaces(value, nameof(value));
+                var duplicates = value.GetNonUniqueNames();
+                if (duplicates.Length > 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"The following skills were specified multiple times: {string.Join(", ", duplicates)}");
+
+                // TODO Need to ensure that added skill does not already exist in BaseSkills or ExceptionalSkills
+                _forbiddenSkills = value;
+            }
+        }
+
+        private void EnsureContainsNoNullOrWhiteSpaces(IReadOnlyList<string> value, string paramName)
+        {
+            if (value.Any(x => x is null || string.IsNullOrWhiteSpace(x)))
+                throw new ArgumentOutOfRangeException(paramName, "NULL or 'All-Whitespace' elements are not allowed!");
+        }
     }
 }
