28c3aefba7611021f1b6ef343b1044708bf27a3e
hummigbird1 <hummigbird1@gmail.com>
2022-10-16 19:35:24 +0200

    Add TotalExperiencePointsTests

---
+++ MidgardLevelHelperCore.Tests/Primitives/TotalExperiencePointsTests.cs
@@ -0,0 +1,20 @@
+﻿using MidgardLevelHelperCore.Primitives;
+using Xunit;
+
+namespace MidgardLevelHelperCore.Tests.Primitives
+{
+    public class TotalExperiencePointsTests
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(uint.MinValue)]
+        [InlineData(uint.MaxValue)]
+        public void ImplicitCastOperator_FromUint_ToTotalExperiencePoints_Works(uint val)
+        {
+            // Implicit Operator call!
+            // Do not change variable declaration to "var"! Otherwise it will no longer be testing the implicit cast operator!
+            TotalExperiencePoints tep = val;
+            Assert.Equal(val, tep.Value);
+        }
+    }
+}
