a5504f2174889ec66593acd1c322a36247aa7fbf
hummigbird1 <hummigbird1@gmail.com>
2022-10-16 19:33:13 +0200

    Add CharacterStatTests

---
+++ MidgardLevelHelperCore.Tests/Primitives/CharacterStatTests.cs
@@ -0,0 +1,17 @@
+﻿using MidgardLevelHelperCore.Primitives;
+using Xunit;
+
+namespace MidgardLevelHelperCore.Tests.Primitives
+{
+    public class CharacterStatTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Ctor_NameCanNotBeNullOrWhiteSpace_ThrowsArgumentNullException(string name)
+        {
+            Assert.Throws<ArgumentNullException>(() => _ = new CharacterStat(name, 1));
+        }
+    }
+}
