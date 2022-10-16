cef6a27ec63e4d2da88cfcc65eec66cf652e3a01
hummigbird1 <hummigbird1@gmail.com>
2022-10-16 19:33:22 +0200

    Add EditableCharacterStatTests

---
+++ MidgardLevelHelperCore.Tests/Primitives/EditableCharacterStatTests.cs
@@ -0,0 +1,17 @@
+﻿using MidgardLevelHelperCore.Primitives;
+using Xunit;
+
+namespace MidgardLevelHelperCore.Tests.Primitives
+{
+    public class EditableCharacterStatTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public void Ctor_NameCanNotBeNullOrWhiteSpace_ThrowsArgumentNullException(string name)
+        {
+            Assert.Throws<ArgumentNullException>(() => _ = new EditableCharacterStat(name, 1));
+        }
+    }
+}
